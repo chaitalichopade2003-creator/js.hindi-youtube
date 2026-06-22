2fc263f1a2d3044e0fd4d28918199e1496544826
chaitalichopade2003-creator <chaitalichopade2003@gmail.com>
2026-06-22 09:18:50 +0000

    variable done

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId = 1
+let accountEmail = "chaitali3252@gmail.accountEmail"
+let accountPassword = 34677878
+ let accountCity = "Pune" 
+let accountState;
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
