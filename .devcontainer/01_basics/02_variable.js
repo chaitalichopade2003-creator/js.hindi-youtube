cb10a4310da6f1ebc15514d74b947ff6295f3766
chaitalichopade2003-creator <chaitalichopade2003@gmail.com>
2026-06-22 13:55:58 +0000

    variable02

---
+++ .devcontainer/01_basics/02_variable.js
@@ -0,0 +1,11 @@
+let score = "Chaitali"
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
